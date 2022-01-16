8aa44f827b679459c79ca6cd281f01738b05cf4b
Shrihari Prakash <shrihariprakash@outlook.com>
2022-01-16 19:29:25 +0530

    - Added excel rows copy functionality.

--- src/components/NewRelease/AlbumPreview/index.js
+++ src/components/NewRelease/AlbumPreview/index.js
@@ -1,10 +1,12 @@
-import { Collapse, Descriptions, Image } from "antd";
+import { Collapse, Descriptions, Image, message } from "antd";
 import { List } from "antd";
 import CollapsePanel from "antd/lib/collapse/CollapsePanel";
 import moment from "moment";
 import React from "react";
 import { TrackListWrapper } from "../TrackList/styles";
+import { PrimaryButton } from "../../common-components/PrimaryButton/styles";
 import { AlbumPreviewWrapper } from "./styles";
+import { copyTextToClipboard } from "../../../utils/clipboard";
 
 export default function AlbumPreview({ album, tracks }) {
   const albumData = [
@@ -20,8 +22,48 @@ export default function AlbumPreview({ album, tracks }) {
     },
   ];
 
+  const copyExcelRows = () => {
+    let excelRows = tracks.map((track, index) => {
+      return [
+        index + 1, // track number
+        track.title, // track title
+        "", // version
+        index + 1, // cd number
+        album.title, // release title
+        track.primaryArtist, // primary artists
+        track.secondaryArtist, // featuring artists
+        "", // remix artists
+        track.lyricist, // authors
+        track.composer, // composers
+        album.label, // label
+        album.productionYear, // production year
+        album.label, // production owner
+        album.label, // copyright owner
+        track.mainGenre, // genre
+        track.subGenre, // sub genre
+        tracks.length === 1 ? "Single" : "Album", // track type
+        track.language, // lyrics language
+        track.language, // title language
+        track.explicit, // parental advisory
+      ];
+    });
+    excelRows = excelRows.map((lines) => lines.join("\t")).join("\n");
+    console.log(excelRows);
+    copyTextToClipboard(excelRows).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+        message.success("Excel rows copied successfully!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+        message.success("Error copying excel rows.");
+      }
+    );
+  };
+
   return (
     <AlbumPreviewWrapper>
+      <PrimaryButton onClick={copyExcelRows}>Copy Excel Rows</PrimaryButton>
       <div className="section-heading">Album Info</div>
       <TrackListWrapper>
         <Image width={200} src={album.artUrl} />
